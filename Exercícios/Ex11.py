448b5f805f935d23e7d1220297a19a4af776dff6
vMorane <54377461+vMorane@users.noreply.github.com>
2019-08-21 21:54:41 -0300

    Add files via upload

---
+++ Exercícios/Ex11.py
@@ -0,0 +1,8 @@
+a = int(input("Digite os anos"))
+m = int(input("Digite os meses"))
+d = int(input("Digite os dias)"))
+
+r = a + m + d
+print("Você tem:", r "de vida.")
+
+
---
+++ Exercícios/Ex13.py
@@ -0,0 +1,8 @@
+a = int (input("Digite o 1º nº:"))
+b = int (input("Digite o 2º nº:"))
+if (a > b):
+    print("Primeiro maior")
+elif: (a = b):
+    print("São iguais")
+else:
+    print("Segundo maior")
